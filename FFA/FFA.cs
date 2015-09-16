24c4c7b8112a19f4493e9976c333edbdcde0e45d
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-09-16 21:24:23 +0300

    Initial

---
+++ FFA/FFA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFA
+{
+    class FFA
+    {
+        List<Firefly> fireflies;
+        double left_border;
+        double right_border;
+
+        double function(List<double> xs)
+        {
+
+
+            return 0;
+        }
+
+        FFA(int number_of_fireflies, double left_border, double right_border)
+        {
+            fireflies = new List<Firefly>(number_of_fireflies);
+            this.left_border = left_border;
+            this.right_border = right_border;
+        }
+
+        public void algorithm()
+        {
+            // Initiaizing population of fireflies
+            List<double> deltas = new List<double>(fireflies[0].coordinates.Count);
+            for (int i = 0; i < deltas.Count; i++)
+
+                //for (int i = 0; i < fireflies.Count; i++)
+                //{ }
+
+        }
+
+    }
+}
---
+++ FFA/Firefly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFA
+{
+    class Firefly
+    {
+        public double attractiveness = 1;
+        public List<double> coordinates;
+
+
+    }
+}
