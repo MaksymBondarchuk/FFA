8ab0a1318257ff32e1ef3e45cabc0960524f9571
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-11-09 23:58:28 +0200

    With start of correct starting configuration

--- FFA/Program.cs
+++ FFA/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main()
         {
-            var fireflyOptimisationAlgorithm = new FireflyOptimisationAlgorithm(30, 10);
+            var fireflyOptimisationAlgorithm = new FireflyOptimisationAlgorithm(50, 2);
 
             fireflyOptimisationAlgorithm.Algorithm();
         }
