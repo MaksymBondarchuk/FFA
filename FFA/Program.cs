fedc11f9733e2a71fddaf9da00556b32fd647642
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-11-11 16:30:10 +0200

    With Rastriigin

--- FFA/Program.cs
+++ FFA/Program.cs
@@ -7,14 +7,14 @@ namespace FFA
     {
         private static void Main()
         {
-            var Rastrigin = new Function
+            var rastrigin = new Function
             {
-                F = (x) => { return x.Sum(t => t*t - 10*Math.Cos(2*Math.PI*t) + 10); },
+                F = x => { return x.Sum(t => t*t - 10*Math.Cos(2*Math.PI*t) + 10); },
                 Range = 5.12,
                 LookingForMax = false
             };
 
-            var fireflyOptimisationAlgorithm = new FireflyOptimisationAlgorithm(50, 2) {FNumber = 2};
+            var fireflyOptimisationAlgorithm = new FireflyOptimisationAlgorithm(50, 2, rastrigin);
 
 
             fireflyOptimisationAlgorithm.Algorithm();
